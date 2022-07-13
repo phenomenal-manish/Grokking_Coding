f396364397e25e3d8a3e5593c6a1f064b7ca7177
phenomenal-manish <mdgawali23@gmail.com>
2022-07-13 12:10:06 -0700

    July 13 Part 1 Sliding Window

--- Sliding_Window/Average_Contiguous_Subarrays_Size_K.py
+++ Sliding_Window/Average_Contiguous_Subarrays_Size_K.py
@@ -1,3 +1,14 @@
+'''
+Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
+
+Let’s understand this problem with a real input:
+
+Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+
+Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+'''
+
+
 # Brute Force 
 '''
 # O(n * k) time | O(n) space
