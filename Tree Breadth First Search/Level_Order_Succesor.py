7b1722df656576c35bb70bca172a93c5e31da9e1
phenomenal-manish <mdgawali23@gmail.com>
2022-08-16 05:48:32 -0700

    Tree Breadth First Search

---
+++ Tree Breadth First Search/Level_Order_Succesor.py
@@ -0,0 +1,64 @@
+'''
+Given a binary tree and a node, find the level order successor of the given node in the tree. 
+The level order successor is the node that appears right after the given node in the level order traversal.
+'''
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+ # O(n) time | O(1) space
+def find_successor(root, key):
+    if root is None:
+        return None
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        currentNode = queue.popleft()
+        # insert the children of current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+
+        # break if we have found the key
+        if currentNode.val == key:
+            break
+
+    return queue[0] if queue else None
+
+
+def main():
+  
+    root = TreeNode(1);
+    root.left = TreeNode(2);
+    root.right = TreeNode(3);
+    root.left.left = TreeNode(4);
+    root.left.right = TreeNode(5);
+  
+    result = find_successor(root, 3)
+    if result:
+        print(result.val)
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+  
+    result = find_successor(root, 9)
+    if result:
+        print(result.val)
+  
+    result = find_successor(root, 12)
+    if result:
+        print(result.val)
+
+
+main()
